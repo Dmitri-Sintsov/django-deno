74f79130e2d72bfc548eabd4403f490a5ce4e8ae
Dmitriy Sintsov <questpc256@gmail.com>
2021-03-20 13:15:43 +0300

    Terminate deno process when management command receives SIGINT signal.

--- django_deno/management/commands/runrollup.py
+++ django_deno/management/commands/runrollup.py
@@ -1,3 +1,6 @@
+import signal
+import threading
+
 from django.conf import settings
 from django.contrib.staticfiles.management.commands import runserver
 
@@ -8,7 +11,8 @@ from ...run import deno_server
 class Command(runserver.Command):
 
     def get_handler(self, *args, **options):
-        deno_server()
+        self.deno_process = deno_server()
+        self.set_sigint_handler()
         """
         Return the static files serving handler wrapping the default handler,
         if static files should be served. Otherwise return the default handler.
@@ -19,3 +23,13 @@ class Command(runserver.Command):
         if use_static_handler and (settings.DEBUG or insecure_serving):
             return RollupFilesHandler(handler)
         return handler
+
+    def set_sigint_handler(self):
+        current_thread = threading.current_thread()
+        main_thread = threading.main_thread()
+        main_thread.join()
+        signal.signal(signal.SIGINT, self.sigint_handler)
+        current_thread.join()
+
+    def sigint_handler(self, signum, frame):
+        self.deno_process.terminate()
