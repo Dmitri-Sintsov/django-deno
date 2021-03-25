ad42bda879e6a3c7a9fa7d41649b96c35c720a8d
Dmitriy Sintsov <questpc256@gmail.com>
2021-03-26 00:20:42 +0300

    Explanation message in sample / testing settings.js module.

--- django_deno/static/django-deno/settings.js
+++ django_deno/static/django-deno/settings.js
@@ -1,3 +1,5 @@
+// Import this module to the project to test automatic importmap generation for virtualenv library modules static file finders.
+
 let DjangoDenoSettings = {
     version: '0.0.1',
 };
