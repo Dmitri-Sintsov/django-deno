70ba7286925f54c07076d219c99ce7014941614a
Dmitriy Sintsov <questpc256@gmail.com>
2021-03-29 18:15:00 +0300

    Added lookahead iterator.

--- django_deno/importmap.py
+++ django_deno/importmap.py
@@ -24,6 +24,22 @@ serialized_map_generator == import_map_generator.serialize()
 import_map == import_map_generator.get_import_map(os.path.join(settings.BASE_DIR, 'drf_gallery', 'static', 'components', 'main.js'))
 """
 
+def lookahead(iterable):
+    """Pass through all values from the given iterable, augmented by the
+    information if there are more values to come after the current one
+    (True), or if it is the last value (False).
+    """
+    # Get an iterator and pull the first value.
+    it = iter(iterable)
+    last = next(it)
+    # Run the iterator to exhaustion (starting from the second value).
+    for val in it:
+        # Report the *previous* value (more to come).
+        yield last, True
+        last = val
+    # Report the last value.
+    yield last, False
+
 
 class CommonBasePath:
 
