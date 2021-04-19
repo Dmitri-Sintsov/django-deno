e7b30fd9d46e91c5779aab9d1bb7c4219b04157d
Dmitriy Sintsov <questpc256@gmail.com>
2021-04-19 22:43:25 +0300

    Refactored collectrollup rollup to Command.rollup_file method.

--- django_deno/management/commands/collectrollup.py
+++ django_deno/management/commands/collectrollup.py
@@ -1,4 +1,3 @@
-import io
 import os
 import ijson
 import json
@@ -24,6 +23,37 @@ class Command(collectstatic.Command, DenoProcess):
         import_map_generator = ImportMapGenerator(logger=self.stderr)
         self.serialized_map_generator = json.dumps(import_map_generator.serialize(), separators=(',', ':')).encode('utf-8')
         self.deno_process = self.run_deno_process()
+        self.rollup_files = []
+
+    def rollup_file(self, path, prefixed_path, source_file):
+        response = DenoRollup(content_type=source_file.content_type).post({
+            'filename': str(source_file.source_path.name),
+            'basedir': str(source_file.source_path.parent),
+            'options': {
+                'relativePaths': True,
+                'staticFilesResolver': True,
+                'terser': True,
+            }
+        })
+        if response.status_code == 200:
+            response_io = IterO(response.streaming_content)
+            # s = response_io.read()
+            objects = ijson.items(response_io, prefix='rollupFile')
+            for obj in objects:
+                if prefixed_path.endswith(obj['filename']):
+                    dest_js_filename = self.storage.path(prefixed_path)
+                    os.makedirs(os.path.dirname(dest_js_filename), exist_ok=True)
+                    dest_map_filename = f"{dest_js_filename}.map"
+                    with open(dest_js_filename, "w") as f:
+                        f.write(obj['code'])
+                        f.write(f"//# sourceMappingURL={obj['filename']}.map)")
+                    with open(dest_map_filename, "w") as f:
+                        f.write(obj['map'])
+                else:
+                    self.terminate(
+                        f"prefixed path {prefixed_path} and generated rollup filename '{obj['filename']}' do not match.")
+        else:
+            self.terminate(f"Error while rollup file {path} {prefixed_path}")
 
     def copy_file(self, path, prefixed_path, source_storage):
         source_path = source_storage.path(path)
@@ -31,33 +61,7 @@ class Command(collectstatic.Command, DenoProcess):
         if source_file.should_rollup():
             if not self.is_local_storage():
                 self.terminate("Only local storage is supported for rollup files")
-            response = DenoRollup(content_type=source_file.content_type).post({
-                'filename': str(source_file.source_path.name),
-                'basedir': str(source_file.source_path.parent),
-                'options': {
-                    'relativePaths': True,
-                    'staticFilesResolver': True,
-                    'terser': True,
-                }
-            })
-            if response.status_code == 200:
-                response_io = IterO(response.streaming_content)
-                # s = response_io.read()
-                objects = ijson.items(response_io, prefix='rollupFile')
-                for obj in objects:
-                    if prefixed_path.endswith(obj['filename']):
-                        dest_js_filename = self.storage.path(prefixed_path)
-                        os.makedirs(os.path.dirname(dest_js_filename), exist_ok=True)
-                        dest_map_filename = f"{dest_js_filename}.map"
-                        with open(dest_js_filename, "w") as f:
-                            f.write(obj['code'])
-                            f.write(f"//# sourceMappingURL={obj['filename']}.map)")
-                        with open(dest_map_filename, "w") as f:
-                            f.write(obj['map'])
-                    else:
-                        self.terminate(f"prefixed path {prefixed_path} and generated rollup filename '{obj['filename']}' do not match.")
-            else:
-                self.terminate(f"Error while rollup file {path} {prefixed_path}")
+            self.rollup_file(path, prefixed_path, source_file)
         else:
             super().copy_file(path, prefixed_path, source_storage)
 
