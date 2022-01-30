3583b45608f4ebe95e3fdd4a8c3364ee4b50b41f
Dmitriy Sintsov <questpc256@gmail.com>
2022-01-31 00:15:12 +0300

    The possibility to optionally override deno lock file path.

--- django_deno/run/base.py
+++ django_deno/run/base.py
@@ -5,7 +5,7 @@ from copy import copy
 
 from django.conf import settings
 
-from ..conf.settings import DENO_PATH, DENO_LOCK_FILE
+from ..conf.settings import DENO_PATH
 
 
 class RunDeno:
@@ -13,6 +13,9 @@ class RunDeno:
     run_flags = ["-A", "--unstable", "--allow-net"]
     script_name = None
     script_args = []
+    # https://deno.land/manual/linking_to_external_code/integrity_checking
+    # https://deno.land/manual/linking_to_external_code
+    deno_lock_filename = 'lock.json'
 
     def get_run_flags(self):
         run_flags = copy(self.run_flags)
@@ -21,9 +24,9 @@ class RunDeno:
             run_flags.append("--inspect-brk")
         # DENO_RELOAD / DENO_CHECK_LOCK_FILE are mutually exclusive options
         if getattr(settings, 'DENO_RELOAD', False):
-            run_flags.extend(["--reload", "--lock-write", f"--lock={DENO_LOCK_FILE}"])
+            run_flags.extend(["--reload", "--lock-write", f"--lock={self.deno_lock_path}"])
         elif getattr(settings, 'DENO_CHECK_LOCK_FILE', False):
-            run_flags.extend([f"--lock={DENO_LOCK_FILE}"])
+            run_flags.extend([f"--lock={self.deno_lock_path}"])
 
         return run_flags
 
@@ -44,6 +47,11 @@ class RunDeno:
         return {
             'shell': False,
         }
+
+    def __init__(self, deno_lock_filename=None, *args, **kwargs):
+        if deno_lock_filename is None:
+            deno_lock_filename = self.deno_lock_filename
+        self.deno_lock_path = os.path.join(settings.BASE_DIR, deno_lock_filename)
 
     def __call__(self, *args, **kwargs):
         shell_args = self.get_shell_args()
