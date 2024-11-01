d79bb55a3e905beb118b23c5c812b8236df54e4c
Dmitriy Sintsov <questpc256@gmail.com>
2024-11-01 22:42:23 +0300

    Fixed compilation of server.ts. Added option for purging of node_modules / vendor dir after deno compile in deno_compile management command.

---
+++ django_deno/dir.py
@@ -0,0 +1,9 @@
+import os, shutil
+
+def del_dir(target_dir):
+    with os.scandir(target_dir) as entries:
+        for entry in entries:
+            if entry.is_dir() and not entry.is_symlink():
+                shutil.rmtree(entry.path)
+            else:
+                os.remove(entry.path)
